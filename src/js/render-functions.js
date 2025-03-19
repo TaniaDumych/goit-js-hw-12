29ee5bb2bd0dee3c872de91cf1687a95aad6f210
Tania Dumych <tanja11.malvchko@gmail.com>
2025-03-19 22:21:16 +0100

    commit js 12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,19 +1,11 @@
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox'; 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 
-const gallery = document.querySelector('.gallery');
-let lightbox;
 export function renderImages(images) {
-  if (!images.length) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Sorry, there are no images matching your search query. Please try again!',
-    });
-    return;
-  }
+  const gallery = document.querySelector('.gallery');
 
   const markup = images
     .map(
@@ -23,24 +15,32 @@ export function renderImages(images) {
             <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
           </a>
           <div class="info">
-            <p><b> Likes:</b> ${likes}</p>
-            <p><b> Views:</b> ${views}</p>
-            <p><b> Comments:</b> ${comments}</p>
-            <p><b> Downloads:</b> ${downloads}</p>
+            <p><b>Likes:</b> ${likes}</p>
+            <p><b>Views:</b> ${views}</p>
+            <p><b>Comments:</b> ${comments}</p>
+            <p><b>Downloads:</b> ${downloads}</p>
           </div>
         </li>
       `
     )
     .join('');
 
-  gallery.innerHTML = markup;
-    if (!lightbox) {
-    lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-  } else {
-    lightbox.refresh();
-  }
+  gallery.insertAdjacentHTML("beforeend", markup); 
+
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250,   
+  });
 }
 
 export function clearGallery() {
-  gallery.innerHTML = '';
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
 }
+
+
+
+
+
+
